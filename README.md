50d53a8ea48e64e34045cab2a39563f4ca6f01b8
husseyexplores <husseyexplores@gmail.com>
2019-09-21 09:46:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CTRL+Enter Image Zoom (Browser Extension)
+If you open an HD (zoomable) image in your browser tab, then you click click the image to zoom in/out. What if you  want to zoom with your keyboard? Well, that is exactly the purpose of this extension.
+
+To toggle the zoom with your keyboard, just press `CTRL + Enter`.
+
+## Installation
+Download links for Chrome and Firefox will be added soon, once the extension is published in the respective stores.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/husseyexplores/image-zoom-extension/blob/master/LICENSE "License") file for details
---
+++ src/inject/inject.js
@@ -0,0 +1,40 @@
+// Debugger
+const debugging = false
+function debug(...args) {
+	if (!debugging) return
+	console.log(...args)
+}
+
+// ---------------------------------------------------------------------------
+
+// Listener
+function handleCtrlEnter(e) {
+	debug('1. Trigger')
+
+	// If the dom has more than 1 elements, then it's must not be image
+	const childElementCount = document.body.childElementCount === 1
+	if (childElementCount > 1) {
+		return
+	}
+
+	// Make sure it's img
+	const img = document.body.querySelector('img')
+	if (!img) {
+		return
+	}
+
+	const isEnterPressed = e.ctrlKey || e.metaKey;
+	const isCtrlPressed = (e.keyCode == 13 || e.keyCode == 10);
+
+	// If CTRL+Enter is pressed, trigger the click on img
+	if (isCtrlPressed  &&  isEnterPressed) {
+		debug('2. Ctrl + Enter')
+		if (img.click) {
+			img.click();
+			debug('3. Clicked')
+		}
+	}
+}
+
+// Start listening
+document.addEventListener('keydown',  handleCtrlEnter);
